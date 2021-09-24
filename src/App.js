2e2e7d5653bc1ca0c71062aced048c00afeb7d2c
arjun <arjun2000raj@gmail.com>
2021-09-24 10:19:05 +0530

    style - styling the app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const geners = ['action', 'comedy', 'drama', 'horror', 'romance', 'thriller'];
 const App = () => {
   const [movieTye, setMovieType] = useState('hollywood');
   const [gener, setGener] = useState('action')
-
+  const [clickedId, setClickedId] = useState(-1);
   // Movie type toggeler function
   const toggleMovieType = () => {
     if (movieTye === 'hollywood') {
@@ -24,65 +24,68 @@ const App = () => {
     <div className="container">
 
       {/* toggle movies type and button */}
-      {movieTye === 'hollywood' ? <button className='toggleBtn' onClick={toggleMovieType}>Bollywood</button> : <button className='toggleBtn' onClick={toggleMovieType}>Hollywood</button>}
 
-      <p>Lets Start :</p>
+      {movieTye === 'hollywood' ? <button className='toggleBtn' onClick={toggleMovieType}>Hollywood</button> : <button className='toggleBtn' onClick={toggleMovieType}>Bollywood</button>}
+      <div className="sub-container">
+        <div className="left">
+          {
+            geners.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  onClick={() => {setGener(item); setClickedId(index)}}
+                  className={index === clickedId ? "customButton active" : "customButton"}
+                >
+                  {item}
+                </button>
+              );
+            })
+          }
+        </div>
+        <div className="right">
+          <p>Movies : </p>
 
-      {
-        geners.map((item, index) => {
-          return (
-            <button
-              key={index}
-              onClick={() => setGener(item)}
-            >
-              {item}
-            </button>
-          );
-        })
-      }
-      <div>
-        <p>Movies : </p>
+          {
+            movieTye === 'hollywood' ?
+              <ul>{
+                // console.log(Array.isArray(moviesCollection[gener].hollywood.movies))
+                moviesCollection[gener].hollywood.movies.map((item, index) => {
+                  return (
+                    <li
+                      key={index}
+                    >
+                      {item}
+                      <span>
+                        &nbsp; {moviesCollection[gener].hollywood.ratings[index]}
+                      </span>
+                    </li>
+                  )
+                })
 
-        {
-          movieTye === 'hollywood' ?
-            <ul>{
-              // console.log(Array.isArray(moviesCollection[gener].hollywood.movies))
-              moviesCollection[gener].hollywood.movies.map((item, index) => {
-                return (
-                  <li
-                    key={index}
-                  >
-                    {item}
-                    <span>
-                      &nbsp; {moviesCollection[gener].hollywood.ratings[index]}
-                    </span>
-                  </li>
-                )
-              })
+              }
 
-            }
+              </ul> :
+              <ul>{
+                // console.log(Array.isArray(moviesCollection[gener].hollywood.movies))
+                moviesCollection[gener].bollywood.movies.map((item, index) => {
+                  return (
+                    <li
+                      key={index}
+                    >
+                      {item}
+                      <span>
+                        &nbsp; {moviesCollection[gener].bollywood.ratings[index]}
+                      </span>
+                    </li>
+                  )
+                })
 
-            </ul> :
-            <ul>{
-              // console.log(Array.isArray(moviesCollection[gener].hollywood.movies))
-              moviesCollection[gener].bollywood.movies.map((item, index) => {
-                return (
-                  <li
-                    key={index}
-                  >
-                    {item}
-                    <span>
-                      &nbsp; {moviesCollection[gener].bollywood.ratings[index]}
-                    </span>
-                  </li>
-                )
-              })
+              }
 
-            }
+              </ul>
+          }
 
-            </ul>
-        }
-
+        </div>
       </div>
     </div>
   )
